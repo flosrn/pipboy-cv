d4ea6d7afef0870ea3430f99dc385f41abf8110e
Flosrn <flosrn.dev@gmail.com>
2024-05-01 06:58:51 +0200

    add pipboy overlay

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      PIPBOY CV
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Image
+        src="/images/Pip-Boy 3000 Mark IV.png"
+        alt="pipboy-overlay"
+        width={1380}
+        height={1047}
+      />
     </main>
   );
 }
