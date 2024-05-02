16bf6a5823caa960a40263be3e7e477a24b23f48
Flosrn <flosrn.dev@gmail.com>
2024-05-02 12:27:41 +0200

    add vaultboy

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,18 +24,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={firaMono.className}>
-        <div className="flex min-h-screen justify-center bg-[#10170f]">
+        <div className="flex min-h-screen justify-center overflow-hidden bg-black">
           <div className="relative mt-20 h-[777px] w-full max-w-5xl xl:h-[900px] xl:max-w-6xl">
             <Image
               src="/images/pipboy/PipBoyOverlay.png"
               alt="PipBoy overlay"
               layout="fill"
               objectFit="cover"
-              className="pointer-events-none z-50 object-[-220px] lg:object-[100px]"
+              className="pointer-events-none z-50 object-[-220px] lg:object-[105px]"
             />
           </div>
 
-          <div className="text-primary pipboy-screen absolute left-0 top-[125px] z-10 mr-3 mt-16 flex h-[350px] w-[395px] flex-col overflow-hidden lg:left-auto xl:top-[150px] xl:h-[400px] xl:w-[500px]">
+          <div className="text-primary pipboy-screen absolute left-0 top-[125px] z-10 mt-16 flex h-[350px] w-full flex-col overflow-hidden min-[380px]:w-[395px] lg:left-auto xl:top-[150px] xl:h-[400px] xl:w-[500px]">
             <NavMenu />
             <div className="text-primary relative h-full">{children}</div>
             <Image
@@ -48,7 +48,7 @@ export default function RootLayout({
               src="/images/pipboy/PipBoyScreenDirt.png"
               alt="PipBoy cracked screen"
               layout="fill"
-              className="pointer-events-none opacity-80"
+              className="pointer-events-none opacity-100"
             />
             <Image
               src="/images/pipboy/PipBoyScreenFingerprint.png"
--- app/stat/status/page.tsx
+++ app/stat/status/page.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+
 export default function StatusPage() {
   return (
-    <div className="h-full text-xs">
-      <div className="absolute bottom-0 left-0 h-16 w-full px-8">bottom</div>
+    <div className="item-center relative flex h-full justify-center text-xs">
+      <Image
+        src="/images/Vaultboy_01.gif"
+        alt="Vaultboy"
+        width={105}
+        height={146}
+        className="absolute inset-0 left-1/2 top-5 -translate-x-1/2 transform"
+      />
+      <div className="absolute bottom-12 left-0 w-full">
+        <div className="mb-1 flex items-center justify-center">
+          Florian SERAN
+        </div>
+        <div className="flex items-center space-x-1 px-8">
+          <div className="bg-secondary px-1">HP 90/90</div>
+          <div className="item-center bg-secondary flex flex-1 items-center space-x-2 px-1">
+            <span className="">LEVEL 30</span>
+            <div className="relative h-2 flex-1">
+              <span className="border-primary absolute inset-0 h-full w-full border-[1px]" />
+              <span className="bg-primary absolute inset-0 h-2 w-1/3" />
+            </div>
+          </div>
+          <div className="bg-secondary px-1">AP 70/70</div>
+        </div>
+      </div>
     </div>
   );
 }
