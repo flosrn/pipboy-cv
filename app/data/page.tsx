92738ad08fc30952230b792301590ff40d8ae689
Flosrn <flosrn.dev@gmail.com>
2024-05-03 12:53:20 +0200

    add under construction

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
+import mining from "../../public/images/vaultboy/build.gif";
+
 export default function DataPage() {
-  return <div className="">Data page</div>;
+  return (
+    <div className="flex h-full flex-col items-center justify-center px-6">
+      <h1 className="font-semibold">Page under construction</h1>
+      <Image
+        src={mining}
+        alt="Under construction"
+        className="special-filter mt-3 w-1/2"
+      />
+      <p className="mt-3 text-center text-xs">Please come back later...</p>
+    </div>
+  );
 }
